5fd7fa4c903250440c1216ac5bcf0328f6fdde20
wkcodes <wkcodes2020@gmail.com>
2021-01-20 14:01:53 -0800

    routes and auth

--- server/routes/api/thing.js
+++ server/routes/api/thing.js
@@ -27,4 +27,22 @@ router.get("/findAllThings", async (req, res) => {
     })
 })
 
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://hargrimm-wikihow-v1.p.rapidapi.com/steps',
+  params: {count: '3'},
+  headers: {
+    'x-rapidapi-key': '57efff2aa5msha7b1ef55b66c789p1cd9fbjsnd90eb24361fb',
+    'x-rapidapi-host': 'hargrimm-wikihow-v1.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
 module.exports = router; 
